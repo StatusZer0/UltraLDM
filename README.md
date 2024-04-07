eaa53557a65c960ca64ba114b4fd0580e2f38b98
Zer0 <statuszero12@gmail.com>
2024-04-07 10:27:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UltraLDM
+
+This is where she makes a mod.
---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GameObject.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(GameObject) 
+{
+	void setVisible(bool v)
+	{
+		if (m_objectType == GameObjectType::Decoration && m_objectID != 44) { // 44 being practice mode checkpoint, because thats a "decoration"
+            GameObject::setVisible(false);
+        } else {
+            GameObject::setVisible(true);
+        }
+	}
+
+	
+};
+
+class $modify(PlayLayer)
+{
+	void addObject(GameObject* obj)
+	{
+		obj->m_activeMainColorID = -1;
+		obj->m_isHide = false;
+
+		PlayLayer::addObject(obj);
+	}
+};
